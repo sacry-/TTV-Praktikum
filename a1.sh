ad3c3742472421ace7488094ac1c2dd8b2e3b712
sacry- <nitsche25@gmail.com>
2017-10-19 14:21:51 +0200

    initial commit, setup of lab

---
+++ a1.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e
+
+# Sensor via RPI: fd16:abcd:ef16:3:D1C1:6D7F:AB6E:1336
+# RPI: FD16:ABCD:EF16:2::1
+
+
+# Main
+sudo /sbin/ifconfig p4p1 up
+sudo /sbin/ifconfig p4p1 inet6 add fd16:abcd:ef16:0002::0002/64
+sudo ip route add fd16:abcd:ef16:3::/64 via fd16:abcd:ef16:2::1
+ping6 -I p4p1 fd16:abcd:ef16:3:D1C1:6D7F:AB6E:1336
+
+
+# 1. Session to Raspberry PI
+ssh pi@FD16:ABCD:EF16:2::1
+# password: TTvSprak
+
+# Address has gone away:
+# sudo systemctl restart lowpan
+ping6 -I lowpan0 fd16:abcd:ef16:3:D1C1:6D7F:AB6E:1336
+
+
+# 2. Session to Raspberry PI
+ssh pi@FD16:ABCD:EF16:2::1
+# password: TTvSprak
+
+# sudo radvd -d 5 -m stderr -n
+# Address has gone away:
+# strg + c
+sudo radvd -d 5 -m stderr -n
+
+
+
