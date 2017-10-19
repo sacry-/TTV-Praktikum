f9077e4ea03acd1c59a97dec926dd6b0dc7d6861
sacry- <nitsche25@gmail.com>
2017-10-19 14:57:59 +0200

    projectschritt 1

--- a1.sh
+++ a1.sh
@@ -10,7 +10,11 @@ set -e
 sudo /sbin/ifconfig p4p1 up
 sudo /sbin/ifconfig p4p1 inet6 add fd16:abcd:ef16:0002::0002/64
 sudo ip route add fd16:abcd:ef16:3::/64 via fd16:abcd:ef16:2::1
-ping6 -I p4p1 fd16:abcd:ef16:3:D1C1:6D7F:AB6E:1336
+
+ping6 fd16:abcd:ef16:3:D1C1:6D7F:AB6E:1336
+
+ssh pi@FD16:ABCD:EF16:2::1 'sudo dumpcap  -P  -i lowpan0  -w -'  | wireshark  -k  -i -
+
 
 
 # 1. Session to Raspberry PI
@@ -22,6 +26,7 @@ ssh pi@FD16:ABCD:EF16:2::1
 ping6 -I lowpan0 fd16:abcd:ef16:3:D1C1:6D7F:AB6E:1336
 
 
+
 # 2. Session to Raspberry PI
 ssh pi@FD16:ABCD:EF16:2::1
 # password: TTvSprak
@@ -30,6 +35,3 @@ ssh pi@FD16:ABCD:EF16:2::1
 # Address has gone away:
 # strg + c
 sudo radvd -d 5 -m stderr -n
-
-
-
