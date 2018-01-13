5b429719a361d9705cbc007f5287876a06468b5b
sacry- <nitsche25@gmail.com>
2018-01-13 13:28:55 +0100

    arithmetic with valueOf

--- eclipse-workspace/battleshipz/src/main/java/battleshipz/battleshipz/Arithmetic.java
+++ eclipse-workspace/battleshipz/src/main/java/battleshipz/battleshipz/Arithmetic.java
@@ -8,60 +8,51 @@ import java.util.Random;
 import de.uniba.wiai.lspi.chord.data.ID;
 
 public class Arithmetic {
-	
+
 	public static final BigInteger MAX_ID = BigInteger.valueOf(2).pow(160).subtract(BigInteger.ONE);
 
-	public static final ID LARGEST_ID = new ID(MAX_ID.toByteArray());
+	public static final ID LARGEST_ID = ID.valueOf(MAX_ID);
 
 	private static ID add(ID id1, ID id2) {
-		return new ID(id1.toBigInteger().add(id2.toBigInteger()).toByteArray());
+		return ID.valueOf(id1.toBigInteger().add(id2.toBigInteger()));
 	}
 
 	private static ID add(ID id1, long value) {
-		return new ID(id1.toBigInteger().add(BigInteger.valueOf(value)).toByteArray());
+		return ID.valueOf(id1.toBigInteger().add(BigInteger.valueOf(value)));
 	}
 
 	private static ID subtract(ID id1, ID value) {
-		return new ID(id1.toBigInteger().subtract(value.toBigInteger()).toByteArray());
+		return ID.valueOf(id1.toBigInteger().subtract(value.toBigInteger()));
 	}
 
 	private static ID subtract(ID id1, int value) {
-		return new ID(id1.toBigInteger().subtract(BigInteger.valueOf(value)).toByteArray());
+		return ID.valueOf(id1.toBigInteger().subtract(BigInteger.valueOf(value)));
 	}
 
 	private static ID multiply(ID id1, long value) {
-//		System.out.println("long value "+ value);
-//		System.out.println(id1.toBigInteger());
-		return new ID(id1.toBigInteger().multiply(BigInteger.valueOf(value)).toByteArray());
+		return ID.valueOf(id1.toBigInteger().multiply(BigInteger.valueOf(value)));
 	}
 
 	private static ID divide(ID id1, long value) {
-		
-		return new ID(id1.toBigInteger().divide(BigInteger.valueOf(value)).toByteArray());
+
+		return ID.valueOf(id1.toBigInteger().divide(BigInteger.valueOf(value)));
 	}
 
 	public static ID mod(ID id1, ID id2) {
-		return new ID(id1.toBigInteger().mod(id2.toBigInteger()).toByteArray());
+		return ID.valueOf(id1.toBigInteger().mod(id2.toBigInteger()));
 	}
 
 	public static ID[] calculateSectors(ID from, ID to, int numberOfFields) {
 		ID[] result = new ID[numberOfFields];
 		ID distance;
-//		System.out.println("numberOfFields" + numberOfFields);
-//		System.out.println("id from: " + from.toBigInteger());
-//		System.out.println("id to: " + to.toBigInteger());
-		
+
 		if (from.compareTo(to) < 0) {
 			distance = subtract(to, from);
 		} else {
 			distance = add(subtract(LARGEST_ID, from), to);
 		}
 
-//		System.out.println("id distance: " + distance.toBigInteger());
-		
-		
 		ID step = divide(distance, numberOfFields);
-//		System.out.println("id step: " + step.toBigInteger());
 		for (int i = 0; i < numberOfFields; i++) {
 			ID id = add(from, 1);
 			ID multiplied = multiply(step, i);
@@ -70,11 +61,11 @@ public class Arithmetic {
 		}
 		return result;
 	}
-	
+
 	public static int isInSector(ID target, ID[] sector) {
 		ID first = sector[0];
 		ID last = sector[sector.length - 1];
-		
+
 		if (!target.isInInterval(first, last)) {
 			return -1;
 		}
@@ -87,7 +78,7 @@ public class Arithmetic {
 		System.out.println("is in sector");
 		return -1;
 	}
-	
+
 	public static ID selectShootID(List<ID> possibleTargets) {
 		Random rnd = new Random();
 		int randIndex = rnd.nextInt(possibleTargets.size());
@@ -95,5 +86,5 @@ public class Arithmetic {
 //		return target;
 		return mod(add(target, 10), LARGEST_ID);
 	}
-	
+
 }
